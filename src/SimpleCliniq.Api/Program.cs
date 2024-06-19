32f6401fe799fe2f07340c3444c034b309385e09
Nielsenprambudi <nielsenprambudiasallie@gmail.com>
2024-06-19 11:41:46 +0700

    update cors

--- src/SimpleCliniq.Api/Program.cs
+++ src/SimpleCliniq.Api/Program.cs
@@ -18,7 +18,7 @@ services.AddCors(options =>
     options.AddPolicy(name: "MyPolicy",
         policy =>
         {
-            policy.WithOrigins("http://localhost:7106",
+            policy.WithOrigins("http://localhost:5173",
                                 "https://52.221.253.49:7000")
                     .WithMethods("PUT", "DELETE", "GET");
         });
