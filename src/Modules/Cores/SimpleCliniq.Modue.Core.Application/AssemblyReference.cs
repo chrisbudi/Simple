fb297304cb88182c6760245a3051a3899117e159
robertjulianto <bertoliant@gmail.com>
2024-10-12 14:09:25 +0700

    adjust core app

---
+++ src/Modules/Cores/SimpleCliniq.Modue.Core.Application/AssemblyReference.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+
+namespace SimpleCliniq.Module.Core.Application;
+
+public static class AssemblyReference
+{
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
