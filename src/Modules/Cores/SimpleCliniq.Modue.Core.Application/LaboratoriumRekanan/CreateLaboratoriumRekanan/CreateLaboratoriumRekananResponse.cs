4d20bfc0a76c8a667ffb02d5fb8c38647bb30fb1
robertjulianto <bertoliant@gmail.com>
2024-10-12 14:05:42 +0700

    crud for laboratorium rekanan

---
+++ src/Modules/Cores/SimpleCliniq.Modue.Core.Application/LaboratoriumRekanan/CreateLaboratoriumRekanan/CreateLaboratoriumRekananResponse.cs
@@ -0,0 +1,5 @@
+using SimpleCliniq.Module.Core.Domain.Models;
+
+namespace SimpleCliniq.Module.Core.Application.LaboratoriumRekanan.CreateLaboratoriumRekanan;
+
+public sealed record CreateLaboratoriumRekananResponse(MLaboratoriumRekanan Data);
