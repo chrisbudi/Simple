070339562316084477798c02afc0446fc7ad3bc9
robertjulianto <bertoliant@gmail.com>
2024-10-20 19:05:17 +0700

    switch to ulid on MHargaRekanan

--- src/SimpleCliniq.Module.Core.Domain/Models/MHargaRekanan.cs
+++ src/SimpleCliniq.Module.Core.Domain/Models/MHargaRekanan.cs
@@ -7,7 +7,7 @@ namespace SimpleCliniq.Module.Core.Domain.Models;
 
 public partial class MHargaRekanan
 {
-    public long IdHargaRekanan { get; set; }
+    public Ulid IdHargaRekanan { get; set; }
 
     public decimal OldIdHargaRekanan { get; set; }
 
