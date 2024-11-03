e37a57e101a2003dc4494e4089458eaa7627657f
Ubuntu <ubuntu@ip-172-31-41-35.ap-southeast-1.compute.internal>
2024-11-03 12:10:41 +0000

    add entrypoin

---
+++ update_ssl_setting.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Start Keycloak in the background
+/opt/keycloak/bin/kc.sh start-dev &
+
+# Wait for Keycloak to start
+echo "Waiting for Keycloak to start..."
+until curl -s http://localhost:8080/auth/realms/master; do
+  sleep 5
+done
+echo "Keycloak has started."
+
+# Use kcadm.sh to update the realm setting
+/opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user "$KEYCLOAK_ADMIN" --password "$KEYCLOAK_ADMIN_PASSWORD"
+/opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE
+
+# Keep the container running
+wait
+
